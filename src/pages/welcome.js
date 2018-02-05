0d670fe5b2484029971c2d98e094e6db1f887af5
wqzone <wqzone@sohu.com>
2018-02-05 19:41:54 +0800

    项目初始化

--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -1,9 +1,14 @@
 import React, {Component} from 'react';
+import {
+    View,
+    Image,
+    Text,
+    StyleSheet
+} from 'react-native';
 import Base from './base'
 import {observer, inject} from 'mobx-react/native';
 import {InputText} from '../components/form/native-base-validate'
 import Form from '../components/form/DataContext'
-import { Container,Header, Content,Text,Spinner} from 'native-base';
 
 @observer
 @inject('welcomeStore','loginStore','welcomeStyle')
@@ -19,12 +24,45 @@ export default class Welcome extends Base{
     render(){
         let { loading } = this.store;
         return (
-            <Container>
-                <Content>
-                    {
-                        loading?<Spinner color="blue"></Spinner>:<Text>旺财宠物</Text>
-                    }
-                </Content>
-            </Container>);
+            <View style={{ flex:1,alignItems:'stretch'}}>
+                <View style={self.imageView}>
+                    <Image
+                        style={self.welcomeImage}
+                        resizeMode="contain"
+                        source={require('../resources/welcome.png')} />
+                </View>
+                <View style={self.logView}>
+                    <Image
+                        style={self.logoImage}
+                        resizeMode="contain"
+                        source={require('../resources/logo-small.png')} />
+                    <Text style={self.title}>旺财宠物</Text>
+                </View>
+            </View>);
     }
 }
+
+let Dimensions = require('Dimensions');
+let { width, height } = Dimensions.get('window');
+let self = StyleSheet.create({
+    imageView:{
+    },
+    welcomeImage:{
+        width:width,
+    },
+    logoImage:{
+        width:40
+    },
+    logView:{
+        flex:1,
+        flexDirection:'row',
+        backgroundColor:'#8BC34A',
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    title:{
+        marginLeft:20,
+        fontSize:22,
+        color:'#ffffff',
+    }
+});
