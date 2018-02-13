f573c3dd883e497e9ec5d62686fa2b4fda510181
wqzone <wqzone@sohu.com>
2018-02-13 16:28:49 +0800

    项目初始化

--- src/components/gest/simple.js
+++ src/components/gest/simple.js
@@ -18,18 +18,18 @@ export default class SimpleGest extends Component{
         let style = this.props.gestSimpleStyle;
         return <View style={[style.body]}>
             <View style={style.sexView}>
-                <Icon name="md-woman" style={style.sex}></Icon>
+                <TouchableOpacity>
+                    <Icon name="ios-call" style={style.sex}></Icon>
+                </TouchableOpacity>
             </View>
             <View style={style.nameView}>
                 <View style={style.name}><Text style={style.nameTxt}>汤姆</Text></View>
-                <TouchableOpacity style={style.phone}>
-                    <Icon name="ios-call" style={style.phoneIcon}></Icon>
+                <View style={style.phone}>
                     <Text style={style.phoneTxt}>182829383828</Text>
-                </TouchableOpacity>
+                </View>
             </View>
             <TouchableOpacity style={style.actionsView}>
                 <Icon name="ios-list" style={style.costIcon} />
-                <Text style={style.costTxt}>费用</Text>
             </TouchableOpacity>
         </View>
     }
