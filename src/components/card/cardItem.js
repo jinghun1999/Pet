0fb08f2820da537a7f2c7e6f8aa89003a79fa2a0
wqzone <wqzone@sohu.com>
2018-02-21 15:19:08 +0800

    项目初始化

--- src/components/card/cardItem.js
+++ src/components/card/cardItem.js
@@ -1,4 +1,8 @@
 import React, {Component} from 'react';
+import {
+    View,
+    TouchableOpacity
+} from 'react-native';
 import {CardItem,Icon,Text,Right,Left,Body} from 'native-base';
 import ItemBase from "./itemBase";
 
@@ -6,11 +10,11 @@ class ExtensionCardItem extends ItemBase{
     constructor(props){
         super(props);
     }
-    render(){
+    renderItem(){
         let {store,label, name,onPress,...props} = this.props;
         let value = store.data[name];
         return (
-            <CardItem {...props}>
+            <CardItem {...props} onPress={ onPress }>
                 <Body style={{ flexDirection:'row' }}>
                     <Text note style={{width:80,textAlign:'right'}}>{label}</Text>
                     <Text>{value}</Text>
@@ -22,6 +26,18 @@ class ExtensionCardItem extends ItemBase{
                 </Right>
             </CardItem>
         )
+    }
+    render(){
+        let {onPress} = this.props;
+        if( onPress ){
+            return <TouchableOpacity onPress={onPress}>
+                {
+                    this.renderItem()
+                }
+            </TouchableOpacity>;
+        }else{
+            return this.renderItem();
+        }
     }
 };
 export default ExtensionCardItem;
